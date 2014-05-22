43df6a131f653f6e115f5fddb4edcca28e4c7173
Sergey Zenchenko <sergeizenchenko@gmail.com>
2014-05-22 15:00:27 +0300

    Add project.

---
+++ JSCore/JCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JCAppDelegate.h
+//  JSCore
+//
+//  Created by Sergey Zenchenko on 5/22/14.
+//  Copyright (c) 2014 Techery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSCoreTests/JSCoreTests.m
@@ -0,0 +1,34 @@
+//
+//  JSCoreTests.m
+//  JSCoreTests
+//
+//  Created by Sergey Zenchenko on 5/22/14.
+//  Copyright (c) 2014 Techery. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface JSCoreTests : XCTestCase
+
+@end
+
+@implementation JSCoreTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
