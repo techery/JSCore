3fdde99c10245ecf1e5f9017ee1d59a54c485a4e
Sergey Stupachenko <sergey.stupachenko@gmail.com>
2014-06-10 16:29:58 +0300

    CAT: cleanup gulpfile

--- brain/gulpfile.js
+++ brain/gulpfile.js
@@ -1,24 +1,22 @@
 require('coffee-script/register');
 
-
-var gulp   = require('gulp'          ),
-    coffee = require('gulp-coffee'   ),
-    util   = require('gulp-util'     ),
-    mocha  = require('gulp-mocha'    ),
+var gulp   = require('gulp'),
+    coffee = require('gulp-coffee'),
+    util   = require('gulp-util'),
+    mocha  = require('gulp-mocha'),
     pkg    = require('./package.json'),
-    clean  = require('gulp-clean'    );
+    clean  = require('gulp-clean');
 
-gulp.task("default", ["clean", "copy-js", "compile-coffee", "copy-npm-dependency"]);
+gulp.task('default', ['clean', 'copy-js', 'compile-coffee', 'copy-npm-dependency']);
 
 gulp.task('copy-js', function() {
- return gulp.src("app/**/*.js")
-    .pipe(gulp.dest("./brain.bundle/"));
+ return gulp.src('app/**/*.js').pipe(gulp.dest('./brain.bundle/'));
 });
 
-gulp.task("compile-coffee", function() {
+gulp.task('compile-coffee', function() {
   return gulp
-          .src(["app/**/*.coffee", "!app/stubs/*.coffee"])
-          .pipe(coffee({bare: true}))
+          .src(['app/**/*.coffee', '!app/stubs/*.coffee'])
+          .pipe(coffee({ bare: true} ))
           .pipe(gulp.dest('./brain.bundle/'))
 });
 
@@ -27,25 +25,25 @@ gulp.task('watch', function() {
 });
 
 gulp.task('spec', function() {
-    return gulp
-            .src(['specs/*.coffee'], { read: false })
-            .pipe(mocha({ reporter: 'list' }))
-            .on('error', util.log);
-});
-
-gulp.task("clean", function(){
   return gulp
-          .src(['./brain.bundle/**'], {read:false})
-          .pipe(clean({force: true}));
+          .src(['specs/*.coffee'], { read: false })
+          .pipe(mocha({ reporter: 'list' }))
+          .on('error', util.log);
 });
 
-gulp.task("copy-npm-dependency", function(){
+gulp.task('clean', function(){
+  return gulp
+          .src(['./brain.bundle/**'], { read: false })
+          .pipe(clean({ force: true }));
+});
+
+gulp.task('copy-npm-dependency', function(){
   var libs = []
   for (var dep in pkg.dependencies) {
-    libs.push("node_modules/" + dep + "/**/*.*");
+    libs.push('node_modules/' + dep + '/**/*.*');
   };
 
   return gulp
-          .src(libs, {base: "./node_modules"})
+          .src(libs, {base: './node_modules'})
           .pipe(gulp.dest('./brain.bundle/node_modules'))
-})
+});
